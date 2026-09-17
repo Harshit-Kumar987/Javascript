aa94c9e25c63e9610f6550e0f0570114709f4413
Harshit-Kumar987 <harshitkumar4568@gmail.com>
2026-09-17 11:19:42 +0530

    Update

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let a= 10;
-console.log(a)
+//console.log("Hello")
+// console.warn("Something went wrong")
+// console.error("error 404")
+
+//var
+var a=10;
+console.log("a=",a)
+var a=20                 // redeclare
+console.log("a=",a)
+a=30
+console.log("a=",a)           //reassign
+
+
+//let
+let b= 12;
+console.log("b=",b)
+//(error) let b=20                 // redeclare
+// console.log("b=",b)
+b=35
+console.log("b=",b)           //reassign
+
+//const
+const c=8;
+console.log("c=",c)
+
+
+var d=40             // scope
+if(true){
+    let e=60;
+    let f=50;
+    console.log(e,f)
+}
